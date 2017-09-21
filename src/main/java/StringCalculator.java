134debae3c4665f14604cab363252940925675a7
Eric Smith <paytonrules@gmail.com>
2017-09-21 13:03:54 -0500

    Remove the if check of on length for a more robust integer check

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,14 +1,20 @@
 public class StringCalculator {
 
-    public int add(String input) {
-        if (input.isEmpty()) {
-            return 0;
+    private boolean isInt(String digit) {
+        try {
+            Integer.valueOf(digit);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
+    }
 
+    public int add(String input) {
         String[] digits = input.split(",");
         int sum = 0;
         for (int i = 0; i < digits.length; i++) {
-            sum += Integer.valueOf(digits[i]);
+            if (isInt(digits[i]))
+                sum += Integer.valueOf(digits[i]);
         }
         return sum;
     }
